da0efffbc922502cc9936db2eb4f1972c14efe41
Gustavo Dantas <gusdantas15@gmail.com>
2024-06-25 15:05:32 -0300

    chore(server): add basic http server

---
+++ apps/server/src/config.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+
+import dotenvSafe from 'dotenv-safe';
+
+const cwd = process.cwd();
+
+const root = path.join.bind(cwd);
+
+dotenvSafe.config({
+  path: root('.env'),
+  sample: root('.env.example'),
+});
+
+const ENV = process.env;
+
+const config = {
+  PORT: ENV.PORT ?? 3000,
+};
+
+export { config };
